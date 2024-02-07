e514bb1a748da2e577932b4b45fac937c36e5f9f
Robert Kazaryan <rk30@gcloud.ua.es>
2024-02-07 21:17:23 +0100

    Fix, Points column to Integer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('points');
+            $table->integer('points');
             $table->rememberToken();
             $table->timestamps();
         });
