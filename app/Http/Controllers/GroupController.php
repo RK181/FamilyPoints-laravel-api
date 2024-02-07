a56c82c3af731a5daf07791e8b29bb5afb396b90
Robert Kazaryan <rk30@gcloud.ua.es>
2024-02-07 17:30:00 +0100

    Add, Update Group

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -40,7 +40,7 @@ class GroupController extends Controller
             $group->creator_id = $user->id;
             $group->name = $request->name;
             $group->points_name = $request->points_name;
-            $group->points_icon = $request->points_icon ? $request->points_icon : false;
+            $group->points_icon = $request->points_icon;
             $group->conf_t_approve = $request->conf_t_approve ? $request->conf_t_approve : false;
             $group->conf_t_validate = $request->conf_t_validate ? $request->conf_t_validate : false;
             $group->conf_t_invalidate = $request->conf_t_invalidate ? $request->conf_t_invalidate : false;
@@ -90,9 +90,79 @@ class GroupController extends Controller
         }
     }
 
+    // Protected Authorization required
     public function updateGroup(Request $request)
     {
-        return '';
+        try{
+            $validateUser = Validator::make($request->all(), 
+            [
+                //'id' => 'required|exists:groups,id',
+                'couple_email' => 'email|exists:users,email',
+                //'rule' => Rule::prohibitedIf($request->user()->id),
+                'points_name' => 'string',
+                'points_icon' => 'string',
+                'conf_t_approve' => 'boolean',
+                'conf_t_validate' => 'boolean',
+                'conf_t_invalidate' => 'boolean',
+                'conf_r_valiadte' => 'boolean'
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'BadRequest',
+                    'errors' => $validateUser->errors()
+                ], 400);
+            }
+
+            $user = $request->user();
+            $group = $user->group;
+            // TO-DO -> Mover la comprobacion a un Middleware
+            if ($group->id == null) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'No group found'
+                ], 404);
+            }
+            
+            if ($request->couple_email != null && $group->couple_id == null) {
+                $group->couple_id = User::where('email', $request->couple_email)->first()->id;         
+            }
+            if ($request->name != null) {
+                $group->name = $request->name;
+            }
+            if ($request->points_name != null) {
+                $group->points_name = $request->points_name;                      
+            }
+            if ($request->points_icon != null) {
+                $group->points_icon = $request->points_icon;                     
+            }
+            if ($request->conf_t_approve != null) {
+                $group->conf_t_approve = $request->conf_t_approve;                     
+            }
+            if ($request->conf_t_validate != null) {
+                $group->conf_t_validate = $request->conf_t_validate;                   
+            }
+            if ($request->conf_t_invalidate != null) {
+                $group->conf_t_invalidate = $request->conf_t_invalidate;          
+            }
+            if ($request->conf_r_valiadte != null) {
+                $group->conf_r_valiadte = $request->conf_r_valiadte;
+            }
+
+            $group->save();
+            
+            return response()->json([
+                'status' => true,
+                'message' => 'Success, Updated Group'
+            ], 200);
+
+        } catch (\Throwable) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Server error'
+            ], 500);
+        }
     }
 
     public function deleteGroup(Request $request)
