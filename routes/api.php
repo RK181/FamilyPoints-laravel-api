e7a294b773791d6111f2f8c9bf3a98b03a0aece6
Robert Kazaryan <rk30@gcloud.ua.es>
2024-03-01 19:18:23 +0100

    Add, middleware

--- routes/api.php
+++ routes/api.php
@@ -28,20 +28,23 @@ Route::post('/auth/login', [AuthController::class, 'logIn']);
 Route::group(['middleware' => ['auth:sanctum']], function () {
     // AUTH
     Route::post('/auth/logout', [AuthController::class, 'logOut']);
+    Route::post('/group/create', [GroupController::class, 'createGroup']);
 
     //Route::resource('group', GroupController::class);
-    Route::post('/group/create', [GroupController::class, 'createGroup']);
-    Route::put('/group/update', [GroupController::class, 'updateGroup']);
-    Route::delete('/group/delete', [GroupController::class, 'deleteGroup']);
-    Route::get('/group', [GroupController::class, 'getGroup']);
-
-
-    Route::get('/group/reward/list', [RewardController::class, 'getGroupRewardList']);
-    Route::post('/reward/create', [RewardController::class, 'createReward']);
-    Route::put('/reward/update', [RewardController::class, 'updateReward']);
-    Route::get('/reward/{id}', [RewardController::class, 'getRewardById'])->whereNumber('id');
-    Route::delete('/reward/delete/{id}', [RewardController::class, 'deleteReward'])->whereNumber('id');
-
+    Route::group(['middleware' => ['GroupExist']], function () {
+        
+        Route::put('/group/update', [GroupController::class, 'updateGroup']);
+        Route::delete('/group/delete', [GroupController::class, 'deleteGroup']);
+        Route::get('/group', [GroupController::class, 'getGroup']);
+        
+        Route::group(['middleware' => ['RewardInGroupExist']], function () {
+            Route::get('/group/reward/list', [RewardController::class, 'getGroupRewardList']);
+            Route::post('/reward/create', [RewardController::class, 'createReward']);
+            Route::put('/reward/update', [RewardController::class, 'updateReward']);
+            Route::get('/reward/{id}', [RewardController::class, 'getRewardById'])->whereNumber('id');
+            Route::delete('/reward/delete/{id}', [RewardController::class, 'deleteReward'])->whereNumber('id');
+        });
+    });
 });
 
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
