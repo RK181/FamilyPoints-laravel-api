33128d16a6e1184773c12a0d42742fc0e6b4923f
Robert Kazaryan <rk30@gcloud.ua.es>
2024-05-01 16:12:16 +0200

    Add InviteNotification class for sending invitation emails

---
+++ app/Notifications/InviteNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InviteNotification extends Notification
+{
+    use Queueable;
+    protected $notification_url;
+    protected $user_name;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('You have been invited to join a group')
+            ->line('User ' + $this->user_name + ' invited you to join the group')
+            ->line('Click the button below to accept the invitation.')
+            ->action('Accept the Invitation', $this->notification_url)
+            ->line('If you do not want to accept the invitation, you can ignore this email.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
