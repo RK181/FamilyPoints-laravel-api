7986d633d83bdb2bf83fcb831048a256c4875500
Robert Kazaryan <rk30@gcloud.ua.es>
2024-05-01 19:39:53 +0200

    Add invite_token column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('invite_token')->nullable();
             $table->string('password');
             $table->integer('points')->default(0);
             $table->rememberToken();
